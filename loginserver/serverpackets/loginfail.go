3a3a55362ebb1accba9f9b9c6042e54fb90b6d7e
frostwind <hi@frostwind.me>
2014-11-20 22:40:13 +0100

    Moved LoginFail and PlayFail consts to consts.go

--- loginserver/serverpackets/loginfail.go
+++ loginserver/serverpackets/loginfail.go
@@ -4,13 +4,6 @@ import (
 	"github.com/frostwind/l2go/packets"
 )
 
-const (
-	REASON_ACCOUNT_IN_USE     = 0x07
-	REASON_ACCESS_FAILED      = 0x04
-	REASON_USER_OR_PASS_WRONG = 0x03
-	REASON_SYSTEM_ERROR       = 0x01
-)
-
 func NewLoginFailPacket(reason uint32) []byte {
 	buffer := new(packets.Buffer)
 	buffer.WriteByte(0x01) // Packet type: LoginFail
--- loginserver/serverpackets/playfail.go
+++ loginserver/serverpackets/playfail.go
@@ -4,11 +4,6 @@ import (
 	"github.com/frostwind/l2go/packets"
 )
 
-const (
-	REASON_TOO_MANY_PLAYERS = 0x0f
-	REASON_ACCESS_FAILED    = 0x04
-)
-
 func NewPlayFailPacket(reason uint32) []byte {
 	buffer := new(packets.Buffer)
 	buffer.WriteByte(0x06) // Packet type: PlayFail
